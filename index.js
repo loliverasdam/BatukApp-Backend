6f785f9ce65845a3d4527df126e85dfaa22b2872
loliverasdam <loliveras.dam@institutcampalans.net>
2023-05-05 16:18:20 +0200

    Port Update

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.set('json spaces', 2)
 app.set('view engine', 'jade')
 app.use(bodyParser.json());
 
+//Listen to the port
+app.listen(5001, () => console.log("BatukApp API runnig in port 5001"))
+
 /* GET home page. */
 app.get('/', (req, res) => {
     res.render('index', { title: 'Express' });
